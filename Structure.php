2e474fd0ff0ed1c4b74c8f884d896cd74bd484f7
ShirleyAiko <50822607+ShirleyAiko@users.noreply.github.com>
2020-02-17 11:26:39 +0800

    Add files via upload

---
+++ Structure.php
@@ -0,0 +1,12 @@
+<form action="/demo/welcome.php" method="post">
+<?php
+echo "<h3>Enter "Y" if finished</h3></br>";
+echo "<h3>Enter "N" if not</h3>";
+?>
+Presentation：<input type="text" name="Presentation"><br>
+Website: <input type="text" name="Website"><br>
+ReadingNotes：<input type="text" name="Reading Notes"><br>
+PM：<input type="text" name="PM"><br>
+<input type="submit">
+
+</form>
